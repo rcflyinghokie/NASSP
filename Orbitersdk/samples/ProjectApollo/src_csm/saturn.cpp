838d0d2afe72ee61984d826ba9e9b1c8cb290def
Ryan <rcflyinghokie@gmail.com>
2024-05-07 11:10:44 -0600

    vesim

--- Orbitersdk/samples/ProjectApollo/src_csm/saturn.cpp
+++ Orbitersdk/samples/ProjectApollo/src_csm/saturn.cpp
@@ -269,7 +269,6 @@ Saturn::Saturn(OBJHANDLE hObj, int fmodel) : ProjectApolloConnectorVessel (hObj,
 	SPSFuelFeedTempSensor("SPS-Fuel-Feed-Temp-Sensor", 0.0, 200.0),
 	SPSOxidizerFeedTempSensor("SPS-Oxidizer-Feed-Temp-Sensor", 0.0, 200.0),
 	SPSEngVlvTempSensor("SPS-Engine-Valve-Temp-Sensor", 0.0, 200.0),
-	vesim(&cbCSMVesim, this),
 	CueCards(vcidx, this, 11)
 #pragma warning ( pop ) // disable:4355
 
