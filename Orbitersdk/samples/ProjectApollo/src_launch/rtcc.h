8b117f15f6164a879e388386f16076fe39df0908
indy91 <indy91@users.noreply.github.com>
2024-05-23 21:40:21 +0200

    New method for MED displays on the RTCC MFD; implement MEDs P13 and P14

--- Orbitersdk/samples/ProjectApollo/src_launch/rtcc.h
+++ Orbitersdk/samples/ProjectApollo/src_launch/rtcc.h
@@ -2717,7 +2717,7 @@ public:
 	//Encke Integrator
 	void EMMENI(EMMENIInputTable &in);
 	//Spherical to inertial conversion
-	int EMMXTR(double GMT, double rmag, double vmag, double rtasc, double decl, double fpav, double az, VECTOR3 &R, VECTOR3 &V);
+	int EMMXTR(double GMT, double rmag, double vmag, double rtasc, double decl, double fpav, double az, int RBI, VECTOR3 &R, VECTOR3 &V);
 	//Orbital Elements Computations
 	void EMMDYNEL(EphemerisData sv, TimeConstraintsTable &tab);
 	//Anchor Vector Maintenance Module
